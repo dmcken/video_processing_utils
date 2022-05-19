4494f5170948d070544aa1fc715cadad5490dc81
David Mc Ken <david.mcken@gmail.com>
2022-05-19 00:38:37 -0400

    Removed trailing whitespace

--- embed_subtitles.py
+++ embed_subtitles.py
@@ -3,11 +3,11 @@ Embed subtitles into the video file:
 
 ffmpeg
     # Video file
-    -i Blitz.2011.1080p.BrRip.mp4 
+    -i Blitz.2011.1080p.BrRip.mp4
     # We are only working with .srt files and permit unicode
     -sub_charenc UTF-8 -f srt
     # SRT file
-    -i Blitz.2011.1080p.BrRip.srt 
+    -i Blitz.2011.1080p.BrRip.srt
     # Copy all the other streams in the video file
     -c copy
     # Embed as a separate stream
