321da414c68d668212d37a3e9e3b0f32268aa6d4
David Mc Ken <david.mcken@gmail.com>
2022-11-25 12:35:24 -0400

    Scaffolding for create subtitles.

---
+++ create_subtitles.py
@@ -0,0 +1,8 @@
+'''
+Create and embed subtitles.
+
+Notes:
+1. From specified file, extract the audio track.
+2. Feed the audio to whisper to create the .srt file.
+3. Use ffmpeg to embed the srt tracks back into the original video track.
+'''
