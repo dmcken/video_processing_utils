4008ff09fd390817fae1aa5a296a6cf1f2bc66ed
David Mc Ken <david.mcken@gmail.com>
2024-09-17 06:03:48 -0400

    Complete most of the cli management for concat_video.

--- src/video_processing_utils/concat_video/cli.py
+++ src/video_processing_utils/concat_video/cli.py
@@ -1,40 +1,86 @@
 '''CLI entry point for concatenation of video.
 '''
+
+# System imports
 import argparse
-import sys
+import logging
+import os
+import pprint
 
+# Local imports
 
-from concat_video import export, users
-from concat_video import users as u
+logger = logging.getLogger(__name__)
 
-def create_parser():
-    '''Arg handler for CLI.
-    '''
+def is_valid_file(parser: argparse.ArgumentParser, filename: str) -> str:
+    """Check for valid input file.
 
+    Args:
+        parser (argparse.ArgumentParser): CLI argparser.
+        filename (str): Filename to check.
+
+    Returns:
+        str: Filename (cleaned).
+    """
+    if not os.path.exists(filename):
+        parser.error(f"File {filename} does not exist")
+        return ""
+    else:
+        return filename
+
+def create_parser() -> argparse.ArgumentParser:
+    """Arg handler for CLI.
+
+    Returns a filled in argument parser.
+
+    Returns:
+        argparse.ArgumentParser: _description_
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', help='the path to the export file')
-    parser.add_argument('--output', default='json', choices=['json', 'csv'], type=str.lower)
+    parser.add_argument(
+        '-i', '--input',
+        default=[],
+        action='append',
+        help='Input files in order to concatenate (minimum two required)',
+        required=True,
+        type=lambda filename: is_valid_file(parser=parser, filename=filename)
+    )
+    parser.add_argument(
+        '-w', '--over-write',
+        help="Overwrite the output file if it exists",
+        default=False,
+        action='store_true',
+    )
+    parser.add_argument(
+        '-o', '--output',
+        help="The output path to output to",
+        required=True,
+    )
+
     return parser
+
+def parse_cli() -> argparse.Namespace:
+    """Return the parsed cli arguments.
+
+    Returns:
+        argparse.Namespace: _description_
+    """
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if len(args.input) <= 1:
+        parser.error("Pass more than 1 input filename")
+
+    if args.over_write is False and os.path.exists(args.output):
+        parser.error(f"Output file '{args.output}' exists, aborting")
+
+    pprint.pprint(args)
+
+    return args
 
 def main():
     '''Main entry point.
     '''
+    args = parse_cli()
 
-
-    args = create_parser().parse_args()
-    users = u.fetch_users()
-
-    if args.path:
-        file = open(
-            args.path,
-            'w',
-            newline='',
-            encoding='utf-8',
-        )
-    else:
-        file = sys.stdout
-
-    if args.format == 'json':
-        export.to_json_file(file, users)
-    else:
-        export.to_csv_file(file, users)
+if __name__ == '__main__':
+    main()
